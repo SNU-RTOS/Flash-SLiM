266485a0fa93800073da6d18c229055fab0599cd
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-03-18 17:25:03 +0900

    minor cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source .env
 cd ${AI_EDGE_TORCH_PATH}
-bazel build -c opt //ai_edge_torch/generative/examples/cpp:text_generator_main 
+bazel build -c opt //ai_edge_torch/generative/examples/rt-llm-infer:text_generator_main 
 
 # bazel build -c opt --copt=-DTFLITE_MMAP_DISABLED //ai_edge_torch/generative/examples/cpp:text_generator_main 
