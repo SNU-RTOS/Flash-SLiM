5a5b8574fe9fec8f5e74764888377b4a89fe7b94
GeonhaPark <geonhab504@gmail.com>
2025-08-30 08:39:31 +0000

    feat: minor code chang -> tools/ebpf

--- tools/ebpf/profile_phase.py
+++ tools/ebpf/profile_phase.py
@@ -3,7 +3,7 @@ from bcc import BPF, USDT
 import atexit, signal, sys, re, os
 
 
-binary_path = "./bin/text_generator_main"
+binary_path = "bin/text_generator_main"
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # ======== Config ========
